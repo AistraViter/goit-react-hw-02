a501e10fc6920a109ded8846985c604a53029d4e
AistraViter <153955169+AistraViter@users.noreply.github.com>
2024-07-11 02:01:28 +0300

    Step6

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Description from "./Description/Description";
 import Options from "./Options/Options";
 import Feedback from "./Feedback/Feedback";
 import Notification from "./Notification/Notification";
 
 export default function App() {
-  const [feedback, setFeedback] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  });
+  const savedFeedback = JSON.parse(window.localStorage.getItem("saved-clicks"));
+
+  const initialFeedback =
+    savedFeedback !== null
+      ? savedFeedback
+      : {
+          good: 0,
+          neutral: 0,
+          bad: 0,
+        };
+
+  const [feedback, setFeedback] = useState(initialFeedback);
+  useEffect(() => {
+    window.localStorage.setItem("saved-clicks", JSON.stringify(feedback));
+  }, [feedback]);
 
   const updateFeedback = (type) => {
     setFeedback((prevFeedback) => ({
